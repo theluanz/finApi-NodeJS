12c09fa39990eaaca3067119bf4f53b8674cddbd
theluanz <luanzuffo.lz@gmail.com>
2022-07-17 16:53:58 -0300

    Create Account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const { v4: uuid } = require('uuid');
 
 const app = express();
+app.use(express.json());
+const customers = [];
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+  const customerAlredyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if (customerAlredyExists) {
+    return res.status(400).json({ message: 'CPF já existe' }).send();
+  }
+
+  customers.push({ id: uuid(), cpf, name, statement: [] });
+
+  return res.status(201).send();
+});
 
 app.listen(3333);
